948b352280231d09d18d258e7f6dde8551a7390a
lisabell-li <lisabell.li@gmail.com>
2018-06-13 20:28:11 +0200

    Added shoppinglist, showing all needed items of all rooms

--- src/app/shoppinglist/shoppinglist.component.ts
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { Room } from '../room';
+import { RoomServiceService } from '../room-service.service';
+
+
 
 @Component({
   selector: 'app-shoppinglist',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppinglistComponent implements OnInit {
 
-  constructor() { }
+  lightbulbCount= 0;
+  lightswitchesCount= 0;
+  socketsCount= 0;
+  windowsCount= 0;
+
+  rooms: Room[] = [];
+  roomCount: Room;
+
+  constructor(private roomService:RoomServiceService) { }
 
   ngOnInit() {
+    this.getRooms();
+  }
+
+  getRooms(): void{
+    this.roomService.getRooms().subscribe( rooms =>this.createShoopingListitems(rooms));
+    
+  }
+
+  createShoopingListitems(roomsSub: Room[]) : void{
+    var i:number; 
+    for(let i=0; i<roomsSub.length; i++){
+      this.lightbulbCount += roomsSub[i].lightbulbs["broken"];
+      this.lightswitchesCount += roomsSub[i].lightSwitches["broken"];
+      this.socketsCount += roomsSub[i].sockets["broken"];
+      this.windowsCount += roomsSub[i].windows["broken"];
+    
+    }
   }
 
 }
